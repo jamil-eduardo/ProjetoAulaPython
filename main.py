ae246fe6104d29c4920e4d2af164386a7307c4cb
Jamil Eduardo <jamil.eduardo@gmail.com>
2024-11-23 16:54:47 -0300

    Meu primeiro arquivo

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de dados")
+
+#Variaveis
+idade = 38
+nome = "Jamil"
+
+#print
+print(idade)
+
+#soma
+nova_idade = idade +1
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+print(lista)
+
+#metodo
+lista_invertida = lista.reverse()
+print(lista_invertida)
